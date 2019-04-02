979edc837390a7c87e9e80cd0dacc5402abc9ed8
shaonq <shaonq@qq.com>
2019-04-02 15:58:45 +0800

    监听网页指定代码

--- bodykeypress.js
+++ bodykeypress.js
@@ -1,13 +1,15 @@
-      !function (a) {
-        function b() {
-          var a = this.global = {list: [], key: ""};
-          return document.body.onkeypress = function (b) {
-            var d, e, f, c = b.key;
-            if (1 === c.length && (a.key += c), "Enter" === c) for (d in a.list) if (e = a.list[d], f = new RegExp(e.code + "$"), f.test(a.key) && "function" == typeof e.func) return e.func()
-          }, this
-        }
-        b.prototype.on = function (a, b) {
-          return this.global.list.push({code: a, func: b}), this
-        }, a.BodyKeypress = b
-        /* demo new BodyKeypress().on('10086',function(){alert('10086拨打成功')}) */
-      }(window);
+  !function (a) {
+    function b() {
+      var a = this.g = {list: [], key: ""};
+      return document.onkeypress = function (b) {
+        var c, d, e, f, g;
+        if (b ? f = b.key : (g = window.event.keyCode, f = 13 === g ? "Enter" : String.fromCharCode(g)), 1 === f.length && (a.key += f), "Enter" === f) for (c in a.list) if (d = a.list[c], e = new RegExp(d.code + "$"), e.test(a.key) && "function" == typeof d.func) return a.key = "", d.func()
+      }, this
+    }
+
+    b.prototype.on = function (a, b) {
+      return this.g.list.push({code: a, func: b}), this
+    }, a.BodyKeypress = b
+
+    /** Demo： new BodyKeypress().on('10086', function () { alert('10086拨打成功') })  */
+  }(window);
